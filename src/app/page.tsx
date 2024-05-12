1401cd549c55a3a1eb6d1340fc2e7dd466f78e44
monica88lima <flima.monica@gmail.com>
2024-05-12 07:53:05 -0300

    pg sobre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import { Clapperboard } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <nav className="flex justify-between bg-slate-800 w-full p-4"> 
       <div className= "flex gap-2 items-center">
-      <Clapperboard className= "text-amber-400" size={28}  />
-      <h1 className="text-amber-400 text-2xl font-bold uppercase"> Senac Filmes</h1>
+        <Clapperboard className= "text-amber-400" size={28}  />
+        <Link href="/"> <h1 className="text-amber-400 text-2xl font-bold uppercase"> Senac Filmes</h1> </Link>
       </div>
-      
-      <a className= "text-gray-400 hover:text-orange-100" href="/sobre"  >Sobre</a>
+      <Link className= "text-gray-400 hover:text-orange-100"href="/sobre"> Sobre </Link>
       </nav>
+      <h2>Filmes em Alta</h2>
       </main>
   );
 }
