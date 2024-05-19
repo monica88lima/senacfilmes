45334e0911205dcf5adbd9845d9ebb19e218d27e
monica88lima <flima.monica@gmail.com>
2024-05-19 09:10:30 -0300

    favoritar

--- src/app/components/cards.tsx
+++ src/app/components/cards.tsx
@@ -1,4 +1,6 @@
-import { Star } from "lucide-react"
+"use client"
+import { Bookmark, BookmarkCheck, Star } from "lucide-react"
+import { useState } from "react";
 interface CardProps{
     filme:{ poster:string,
         titulo: string,
@@ -9,8 +11,21 @@ interface CardProps{
 }
 
 export function Cards({filme}:CardProps){
+   const[favorito, setFavorite]= useState(false)
+   function favoritar(){
+    const favoritos =JSON.parse(localStorage.getItem("favoritos")) || []
+    favoritos.push(filme)
+    localStorage.setItem("favoritos",JSON.stringify(favoritos))
+    setFavorite(!favorito)
+   }
+
     return (
-        <div className="flex flex-col gap-2 items-center w-40 m-4">
+        <div className="flex flex-col gap-2 items-center w-40 m-4 relative">
+        {favorito ? 
+        <BookmarkCheck onClick={favoritar}  className="absolute right-2 top-2 cursor-pointer text-amber-300"/>:
+        <Bookmark onClick={favoritar}color="#faf9f9" className="absolute right-2 top-2 cursor-pointer" />
+
+        }                    
             <img className="rounded-lg"src={filme.poster} alt="fotoFilme" />
             <span className="text-lg font-medium line-clamp-1">
             {filme.titulo}</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,28 @@ import { Cards } from "./components/cards";
 import { TitleArticle } from "./components/titleArticle";
 
 export default function Home() {
-  const filme1 = {
+  const filmes = [{
     poster: "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/dzDK2TMXsxrolGVdZwNGcOlZqrF.jpg",
     titulo: "Planeta dos Macacos: O Reinado ",
     nota: 7.5
-  }
-  const filme2 = {
+  },
+  {
     poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/rC5RqXtFoTkBhNWE1dczwi4dZrX.jpg",
     titulo: "Garfield - Fora de Casa ",
     nota: 6.5
+  },
+  {
+    poster: "https://media.themoviedb.org/t/p/w220_and_h330_face/fWSGD2yrzz6hscocnMD8AEXIThk.jpg",
+    titulo:"Godzilla e Kong - O novo Império",
+    nota: 7.2
+  },
+  {
+    poster: "https://media.themoviedb.org/t/p/w220_and_h330_face/aNK6MA5EApIo0UJE7ZWSYcZBJKy.jpg",
+    titulo:"Kung Fu Panda 4",
+    nota: 7.1
   }
+]
+  
 
 
   return (
@@ -25,9 +37,8 @@ export default function Home() {
       <NavBar />
       <Title>Filmes em Alta</Title>
       <section className="flex flex-wrap gap-4 m-4">
-        <Cards filme={filme1}></Cards>
-        <Cards filme={filme2}></Cards>
-
+        {filmes.map(filme => <Cards filme={filme}/>)}
+        
       </section>
 
       <Title>Séries em Alta</Title>
